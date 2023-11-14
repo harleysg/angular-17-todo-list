516274ffea8be61c521a7bc9d10e964f19d81b3e
harley santos garzon <harley.santos.garzon@gmail.com>
2023-11-13 23:34:07 -0500

    refactor(app): use layout

--- projects/todo-app/src/app/app.component.ts
+++ projects/todo-app/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { LayoutComponent } from '@shared/index';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LayoutComponent],
   schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
---
+++ projects/todo-app/src/shared/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './page-not-found/page-not-found.component'
+export * from './salut/salut.component'
+export * from './todo/todo.component'
